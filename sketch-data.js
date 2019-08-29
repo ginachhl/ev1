d7be4d54a84764b5e767bc7e7b6d4038eb0f59c1
ginachhl <53581738+ginachhl@users.noreply.github.com>
2019-08-29 17:29:58 -0400

    Update sketch-data.js

--- sketch-data.js
+++ sketch-data.js
@@ -1,16 +1,20 @@
+var temblores;
+
 function preload(){
 	temblores = loadJSON ('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson')}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  console.log(temblores);
+  print(temblores);
+      for (var x = 0; x < temblores.features.length; x++)
+      if(temblores.features[x].properties.place.includes("Chile"))
+        createP(temblores.features[x].properties.mag);
 }
 
 function draw() {
-	if temblores.features.length > 5;
   colorMode(HSB, 360, 100, 100); 
-  for (var x = 0; x < 1; x++) {
+  for (var x = 0; x < temblores.features.length; x++) {
     var a = random(120, 240);
     var b = random(0, 100);
     var c = random(50,100);
@@ -18,8 +22,9 @@ function draw() {
     pelota(a);
   }
 }
-
+        
 function pelota(diametro) {
   square(random(0, width), random(0, height), diametro);
 }
 
+  
