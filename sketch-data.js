eb31390d03f43df4a81d23af5c5e95beb1cfb79a
ginachhl <53581738+ginachhl@users.noreply.github.com>
2019-09-12 17:31:08 -0300

    Update sketch-data.js

--- sketch-data.js
+++ sketch-data.js
@@ -1,12 +1,11 @@
 var datos;
 
 function preload() {
-	datos = loadTable('https://raw.githubusercontent.com/ginachhl/ev1/gh-pages/datos.csv', 'csv', 'header') }
+	datos = loadJson('http://api.myjson.com/bins/1gvg1d') }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	console.log(datos);
-	console.log(datos.get(1,0));
+	console.log(datos[0]["mom"]);
 }
 
 function draw() {
